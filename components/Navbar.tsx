fb93937e9ea93e1011e7a8716ab7653e932ae8a2
Edward Marzal <edsanchezmarzal@gmail.com>
2022-08-18 13:13:01 -0500

    adding logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,18 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktak-logo.png';
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href={''}>
+    <div className="w-full flex justify-between">
+      <Link href="/">
         <div>
           <Image  
             className='cursor-pointer'
-            src={  }
+            src={ Logo }
+            alt="TikTak"
+            layout="responsive"
           />
         </div>
       </Link>
