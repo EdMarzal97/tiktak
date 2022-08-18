b0a0f01006a5a90da06a9c1804a0c4c2b154e893
Edward Marzal <edsanchezmarzal@gmail.com>
2022-08-18 13:16:06 -0500

    navbar base

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,9 +10,9 @@ import Logo from '../utils/tiktak-logo.png';
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div>
+        <div className='w-[100px]'>
           <Image  
             className='cursor-pointer'
             src={ Logo }
